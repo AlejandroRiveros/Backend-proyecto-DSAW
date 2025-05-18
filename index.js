2613e22266763e780aeb55ab432a5ad5243a86df
AlejandroRiveros <alejandroriso@unisabana.edu.co>
2025-05-18 18:30:00 -0500

    errores

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 3001;
 const Order = require('./Order');
 
 // Configuración de CORS para Express
-const allowedOrigin = process.env.CORS_ORIGIN || 'https://frontend-dsaw.vercel.app/';
+const allowedOrigin = process.env.CORS_ORIGIN || 'https://frontend-dsaw.vercel.app';
 app.use(cors({
   origin: allowedOrigin,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
@@ -33,7 +33,11 @@ app.options('*', cors());
 app.use(express.json());
 
 // Conexión a MongoDB Atlas
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect('mongodb+srv://alejandrorivsob:Majo1811@alejo18.znsakxl.mongodb.net/InventoryDB?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000, // Aumentar el tiempo de espera a 30 segundos
+})
   .then(() => console.log('Conexión exitosa a MongoDB Atlas'))
   .catch((error) => console.error('Error al conectar a MongoDB Atlas:', error));
 
